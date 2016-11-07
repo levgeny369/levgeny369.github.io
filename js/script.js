90d1548411cb23b2d85e2a1c873a0663821efd18
levgeny369 <levgeny369@gmail.com>
2016-11-07 09:50:39 +0300

    Настраиваю гамбургер

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function secret(id_div) {
+if (document.getElementById(id_div).style.display == "block")
+document.getElementById(id_div).style.display = "none";
+else
+document.getElementById(id_div).style.display = "block";
+};
