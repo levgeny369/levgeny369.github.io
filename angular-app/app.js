0e1c0647f1f89eac4f42bee4d53516df7b965f78
Levgeny369 <levgeny369@gmail.com>
2016-12-04 20:35:42 +0300

    ok

--- angular-app/app.js
+++ angular-app/app.js
@@ -14,9 +14,10 @@
          $scope.galery_mini2 = data.features2;
          $scope.meet_img_mini = data.meet_img_mini;
          $scope.some_img_mini = data.some_img_mini;
-         $scope.menu = data.menu;
+         // $scope.menu = data.menu;
+         $scope.json = data;
 
-         some_img_mini
+         
     
          // console.log('\n\nData.iphone_block1: ', data.iphone_block2);
          // console.log('\n\nData.iphone_block1: ', $scope.iblocks2);
@@ -24,5 +25,75 @@
          console.log('error');
      });
 
-  
+    // $scope.blocks = [
+    //   {
+    //       "src": "./images/features/1.png",
+    //       "features_mini2": "Branding",
+    //      "features_text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore  "
+    //     },
+    //      {
+    //      "src": "./images/features/2.png",
+    //      "features_mini2": "Development",
+    //      "features_text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore  "
+    //    },
+    //    {
+    //       "src": "./images/features/3.png",
+    //      "features_mini2": "Consuting",
+    //      "features_text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore  "
+    //    }
+    // ]
   });
+
+
+
+  // var app = angular.module('dtwoshop',[]);
+
+  // app.controller('dtwoNavbarController', ['$http', function($http) {
+  //   this.links = [];
+  //   var menu = this;
+  //   $http.get("/json/server.json").then(
+  //     function(res){
+  //       menu.blocks = res.data.blocks;
+  //     },
+  //     function(res) {
+  //       console.log("links error " + res);
+  //       menu.blocks = [
+  //         {          
+  //           "link": "#",
+  //           "features_mini2": "Branding"
+          
+  //         }
+  //       ];
+  //     }
+  //   );
+  // }]);
+  // app.directive('dtwoMain', ['$http', function($http){
+  //   return {
+  //     restrict: 'E',
+  //     templateUrl: '/angular-app/templates/main.tpl.html',
+  //     controller: function(){
+  //       this.activeTab = 0;
+  //       this.tabs = [];
+  //       var about = this;
+  //       this.setTab = function(activeTab, $event) {
+  //         $event.preventDefault();
+  //         if (activeTab)
+  //           this.activeTab = activeTab;
+  //         else this.activeTab = 0;
+  //       };
+  //       this.isActive = function(tab) {
+  //         return tab == this.activeTab;
+  //       };
+  //       $http.get("/json/server.json").then(
+  //       function(res){
+  //         about.tabs = res.data.about;
+  //       },
+  //       function(res) {
+  //         console.log("links error " + res);   
+  //       });
+  //     },
+  //     controllerAs: 'about'
+  //   };
+  // }]);
+
+// })();
--- js/script.js
+++ js/script.js
@@ -4,3 +4,54 @@ document.getElementById(id_div).style.display = "none";
 else
 document.getElementById(id_div).style.display = "block";
 };
+
+
+// localStorage.setItem('name', '1');
+
+// function generateAllProductsHTML(data,idTemplate,boxClass){
+	
+// 	var theTemplate = Handlebars.compile ($(idTemplate).html());
+// 	$(boxClass).append (theTemplate(data));			 
+// }
+$(function () {
+	
+	// var pictures = [];
+
+	// $.ajax('./json/base.json').done(function(data) {
+	// 	pictures = data.features1;
+
+	// 	generateAllProductsHTML(data,'#sfeatures_mini','#features');
+	// 	generateAllProductsHTML(data,'#sgalery_img','#galery_img_1');
+	// 	generateAllProductsHTML(data,'#sgalery_img2','#galery_img_2');
+	// 	generateAllProductsHTML(data,'#siphone_block1','#block_big1');
+	// 	generateAllProductsHTML(data,'#siphone_block2','#block_big2');
+	// 	generateAllProductsHTML(data,'#smeet_img_mini','#meet_img');
+	// 	generateAllProductsHTML(data,'#ssome_img_mini','#some_img');
+
+	// }).fail(function(data) {
+
+	// });
+
+ 	$('body').on('click','.galery_hover',function(e) {
+ 		e.preventDefault()
+ 		$('.single-picture').addClass('visible');
+
+		var index = $(this).attr('index');
+		for (var i = 0; i  < pictures.length; i++) {
+			// console.log(pictures.length)
+			// console.log(pictures[i].id==index)
+			console.log(pictures[i].id);
+			console.log(index);
+			if(pictures[i].id == index){
+				$('.preview-large #page_img').attr('src',pictures[i].src);
+				$('.preview-large h3').text(pictures[i].text);
+				$('.preview-large p').text(pictures[i].inf);
+			}
+		}
+	});
+	$(".overlay, .close").on('click',function(e) {
+		$('.single-picture').removeClass('visible');
+	})
+
+
+});
